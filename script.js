1b2ec22b8fa1112b56456b6c8d3e9bf79233510e
Gabriela Souza <arq.gabrielasouza@gmail.com>
2022-01-16 20:05:40 -0300

    Consumir a API

--- script.js
+++ script.js
@@ -17,5 +17,62 @@ const condicao3 = document.querySelector('#status3');
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
+}
 
-    
+
+pegarPersonagem1 = () => {
+    let numeroAleatorioPersonagem1 = gerarValorAleatorio()
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioPersonagem1}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem1.src = data.image;
+        imagem1.alt=data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML= data.status;
+    });
+
+
+}
+
+pegarPersonagem2 = () => {
+    let numeroAleatorioPersonagem2 = gerarValorAleatorio()
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioPersonagem2}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt=data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML= data.status;
+    });
+
+
+}
+
+pegarPersonagem3 = () => {
+    let numeroAleatorioPersonagem3 = gerarValorAleatorio()
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioPersonagem3}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;
+        imagem3.alt=data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML= data.status;
+    });
+
+
+}

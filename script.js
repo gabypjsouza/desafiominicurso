c8e924a0b2d00bb2c31243b98dd2c32f0b564e1f
Gabriela Souza <arq.gabrielasouza@gmail.com>
2022-01-16 19:51:25 -0300

    Criar const no script

---
+++ script.js
@@ -0,0 +1,16 @@
+const botao = document.querySelector('#botao');
+
+const imagem1 = document.querySelector('#personagem1');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+
+const imagem2 = document.querySelector('#personagem2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+
+const imagem3 = document.querySelector('#personagem3');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
